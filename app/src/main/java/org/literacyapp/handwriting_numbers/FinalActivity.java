ae10f074143b25e601b6dc01dbe65130dac9b979
Tuan Nguyen <tuancoltech@gmail.com>
2026-05-22 11:03:44 +0700

    Fix build errors

--- app/src/main/java/org/literacyapp/handwriting_numbers/FinalActivity.java
+++ app/src/main/java/org/literacyapp/handwriting_numbers/FinalActivity.java
@@ -5,7 +5,7 @@ import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
